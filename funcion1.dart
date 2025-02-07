a8fdaad0e10e0e5cdd0d64ba5a58e12bc34fe775
JToledo19 <167555295+JToledo19@users.noreply.github.com>
2025-02-07 16:50:57 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+// Funcion que no devuelve valor
+// funcion saludar
+void saludar(){
+  print('hola mundo');
+}
+void escribirNombre(){
+  print('Jorge Toledo');
+}
+
+void main(){
+  //Jorge Toledo Mat: 22308051281106
+  //llamar a la funcion saludar 
+  print('Jorge Toledo Mat: 22308051281106');
+  print('llamar a la funcion saludar ');
+  saludar();
+  print('llamar a la funcio escribirNombre');
+  escribirNombre();
+
+}
